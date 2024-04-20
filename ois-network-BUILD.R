3576e8635ec3f3ebc7241ecc78170cb673008a59
jacobtnyoung <CRJ605@asu.edu>
2024-04-20 10:28:06 -0700

    edits to build file

--- ois-network-BUILD.R
+++ ois-network-BUILD.R
@@ -1,6 +1,8 @@
 # ************************************** #
 # This file pulls the OIS data and builds bipartite networks from those data
+# ************************************** #
 
+# ************************************** #
 
 # ----
 # clear workspace and load libraries
@@ -154,9 +156,15 @@ gplot( netOIS,
 )
 
 
-# ----
-# Integrate hand cleaned data file
+# ************************************** #
+# Build the network for the hand cleaned data file
 
+# for this section I just recycle code from above
+# a more efficient way to do this would be to write a function
+# or a loop, but we will not do that right here
+
+
+# get the file
 dat2 <- read_xlsx( 
   here( "OIS-DATA-EDITED.xlsx" ), # call the local directory
   na = "NA"                       # set character for missing values
@@ -167,10 +175,6 @@ names( dat2 )
 
 # check the counts of duplicate incident ids
 table( table( dat2$INC_RPT ) )
-
-
-# ----
-# clean the 
 
 
 # ----
@@ -191,10 +195,115 @@ dat.ois2 <- dat.ois2 %>%
   na.omit()
 
 
+# check again
+table( is.na( dat.ois2$REFERENCE_ID ) )
 
 
+# ----
+# create the actor and event as characters
+
+dat.ois2 <- dat.ois2 %>% 
+  mutate( actor = paste0( "a.", dat.ois2$REFERENCE_ID ) ) %>%  # change to character
+  mutate( event = paste0( "p.", dat.ois2$INC_RPT ) )           # change to character
 
 
+# ----
+# create the edgelist  
+
+dat.edgelist2 <- dat.ois2 %>% 
+  select( actor, event ) %>%  # take the actor and event 
+  arrange( actor )            # sort by the actor
+
+
+# ----
+# check the data and create objects
+
+# check duplicate entries
+table( duplicated( dat.edgelist2 ) )
+
+# check dimensions
+dim( dat.edgelist2 )
+
+# check number of officers
+n.actors2 <- dim( unique( dat.edgelist2[,1] ) )[1]
+n.actors2
+
+# check number of incidents
+n.events2 <- dim( unique( dat.edgelist2[,2] ) )[1]
+n.events2
+
+
+# ----
+# construct the network
+
+# build the edgelist
+mat.edgelist2 <- cbind( 
+  dat.edgelist2$actor, 
+  dat.edgelist2$event 
+)
+
+
+# create the network
+netOIS2 <- as.network(                             
+  mat.edgelist2, 
+  bipartite=n.actors2, 
+  matrix.type="edgelist"
+)
+
+
+# ----
+# attach attributes
+
+# create the attributes object
+dat.attrs2 <- dat.ois2 %>% 
+  select( actor, PO_GENDER ) %>% 
+  group_by( actor ) %>%
+  distinct() %>% 
+  arrange( actor )
+
+
+# check names match 
+id.check2 <- cbind( 
+  dat.attrs2$actor, 
+  network.vertex.names( netOIS2 )[1:n.actors2] 
+)
+
+
+# match?
+table( id.check2[,1] == id.check2[,2] )
+
+
+# merge the officer gender attribute to the data
+gender2 <- dat.attrs2$PO_GENDER
+gender2 <- c( gender2, rep( NA, n.events2 ) )
+
+
+# assign the gender variable to the network
+netOIS2 %v% "gender" <- gender2
+
+
+# ----
+# plot the network
+
+# set the colors
+col.gender2 <- netOIS2 %v% "gender"
+col.gender2[ netOIS2 %v% "gender" == "Male" ] <- "lightblue"
+col.gender2[ netOIS2 %v% "gender" == "Female" ] <- "#fc03c2"
+col.gender2[ is.na( netOIS2 %v% "gender") ] <- "black"
+
+
+# plot it
+gplot( netOIS2,
+       gmode = "twomode",
+       usearrows = FALSE,
+       vertex.col = col.gender2,
+       edge.col = "grey60",
+       main = "OIS Incidents Network\n (color reflects gender)"
+)
+
+
+# ************************************** #
+# Integrate the two networks
 
 
 
